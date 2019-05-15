3435cd1c960c88fc5b45e6ab7bcc30687b5575fe
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-15 21:55:33 +0800

    add destination supplier

---
+++ app/src/main/java/au/com/hyperionstudios/travelapp/datasupplier/CustomerSupplier.java
@@ -0,0 +1,13 @@
+package au.com.hyperionstudios.travelapp.datasupplier;
+
+import java.util.List;
+
+import au.com.hyperionstudios.travelapp.model.Customer;
+
+public class CustomerSupplier {
+
+    public static List<Customer> getCustomers(){
+        return null;
+    }
+
+}
---
+++ app/src/main/java/au/com/hyperionstudios/travelapp/datasupplier/DestinationSupplier.java
@@ -0,0 +1,13 @@
+package au.com.hyperionstudios.travelapp.datasupplier;
+
+import java.util.List;
+
+import au.com.hyperionstudios.travelapp.model.Destination;
+
+public class DestinationSupplier {
+
+    public static List<Destination> getDestinations(){
+        return null;
+    }
+
+}
